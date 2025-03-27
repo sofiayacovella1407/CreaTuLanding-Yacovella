6983db073396ed6fc82f98394335016e37e3d18c
Sofia Yacovella <sofiayacovella@outlook.com>
2025-03-27 15:36:16 -0300

    NavegaLasRutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import CategoryContainer from "./components/CategoryContainer";
 
 const App = () => {
   return (
@@ -9,6 +11,9 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<ItemListContainer mensaje="Bienvenido a nuestra tienda de servicios" />} />
+        <Route path="/category/:categoryId" element={<CategoryContainer />} />
+        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+        <Route path="*" element={<div>404 - Página no encontrada</div>} />
       </Routes>
     </Router>
   );
